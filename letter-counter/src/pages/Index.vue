d9c9662f229ce807e7cb967d7f9d58c8758afe8e
Elias Shirouyeh <37978575+mrilia@users.noreply.github.com>
2021-08-17 21:44:06 +0430

    add text len counter

--- letter-counter/src/pages/Index.vue
+++ letter-counter/src/pages/Index.vue
@@ -1,13 +1,24 @@
 <template>
-  <q-page class="flex flex-center">
-    
+  <q-page padding>
+    <input v-model="text" @keyup="calculateLen()" />
+    <p class="text-grey2">Letter count: {{ textLen }}</p>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  
-})
+  data() {
+    return {
+      text: "",
+      textLen: 0,
+    };
+  },
+  methods: {
+    calculateLen() {
+      this.textLen = this.text.length;
+    },
+  },
+});
 </script>
