4967b2db43a1507f2a7cabccacc47ef29f37c8c0
Elias Shirouyeh <37978575+mrilia@users.noreply.github.com>
2021-08-17 19:10:13 +0430

    add button to change color

--- background-color-changer/src/pages/Index.vue
+++ background-color-changer/src/pages/Index.vue
@@ -1,17 +1,33 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page
+    class="flex flex-center"
+    v-bind:style="{'background-color':colors[colorIndex]}"
+  >
+    <q-btn
+      @click="setNextColor()"
+      rounded
+      color="black"
+      label="Click to change color"
+    />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'PageIndex'
-})
+  data() {
+    return {
+      colorIndex: 0,
+      colors: ["#B29DD9", "#FDFD98", "#FE6B64", "#77DD77", "#779ECB"],
+    };
+  },
+  methods: {
+    setNextColor() {
+      this.colorIndex == this.colors.length - 1
+        ? (this.colorIndex = 0)
+        : this.colorIndex++;
+    },
+  },
+});
 </script>
